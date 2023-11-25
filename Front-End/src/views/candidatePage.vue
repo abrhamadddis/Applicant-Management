671c39209d023c4eae57317a4d45707920c6ef33
abrhamadddis <abrhamaddis32@gmail.com>
2023-11-25 14:36:38 +0300

    user detail feature is complated

--- Front-End/src/views/candidatePage.vue
+++ Front-End/src/views/candidatePage.vue
@@ -115,50 +115,66 @@
           </v-card>
         </v-dialog>
           <!-- user information  -->
-        <v-dialog v-model="infoDialog" max-width="1000px">
+        <v-dialog v-model="infoDialog" max-width="1100px" >
           <template v-slot:activator="{ props }">
           </template>
-          <v-card>
+          <v-card class="px-8 py-8">
             <v-card-title>
-              <span class="text-h5">Item Information</span>
+              <span class="text-h5">Candidate Profile</span>
             </v-card-title>
             <v-col>
-              <v-row>
-                <v-col>
-                  <v-img
-                    src="https://cdn.vuetifyjs.com/images/john.png"
-                    alt="John"
-                    class="w-50"
-                  ></v-img>
-                </v-col>
-                <v-col>
-                  <p>First Name: {{ clickedItem.firstName }}</p>
-                  <p>Last Name: {{ clickedItem.lastName }}</p>
-                  <p>Foreign Name: {{ clickedItem.foreignName }}</p>
-                </v-col>
-                <v-col>
+              <v-row >
+                <v-col cols="3">
+                      <v-row>
+                        <svg xmlns="http://www.w3.org/2000/svg" height="5rem" viewBox="0 0 448 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M224 256A128 128 0 1 0 224 0a128 128 0 1 0 0 256zm-45.7 48C79.8 304 0 383.8 0 482.3C0 498.7 13.3 512 29.7 512H418.3c16.4 0 29.7-13.3 29.7-29.7C448 383.8 368.2 304 269.7 304H178.3z"/></svg>
+                        <v-col>
+                      <p>First Name: {{ clickedItem.firstName }}</p>
+                      <p>Last Name: {{ clickedItem.lastName }}</p>
+                      <p>Foreign Name: {{ clickedItem.foreignName }}</p>
+                    </v-col>
+                </v-row>
+              </v-col>
+
+              <v-col>
                   <p> Applied For: {{ clickedItem.position }}</p>
                   <p>Desired salary: 500</p>
-                </v-col>
-              </v-row>
-              <v-row>
+                  <p>Current Client: {{ clickedItem.currentClient }}</p>
+              </v-col>
+              <v-col>
+                  <v-row>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 384 512"><path d="M215.7 499.2C267 435 384 279.4 384 192C384 86 298 0 192 0S0 86 0 192c0 87.4 117 243 168.3 307.2c12.3 15.3 35.1 15.3 47.4 0zM192 128a64 64 0 1 1 0 128 64 64 0 1 1 0-128z"/></svg>
+                    <p class="pl-2">{{ clickedItem.location }}</p>
+                  </v-row>
+                  <v-row>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z"/></svg>
+                    <p class="pl-2">{{ clickedItem.email }}</p>
+                  </v-row>
+                  <v-row>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 512 512"><path d="M164.9 24.6c-7.7-18.6-28-28.5-47.4-23.2l-88 24C12.1 30.2 0 46 0 64C0 311.4 200.6 512 448 512c18 0 33.8-12.1 38.6-29.5l24-88c5.3-19.4-4.6-39.7-23.2-47.4l-96-40c-16.3-6.8-35.2-2.1-46.3 11.6L304.7 368C234.3 334.7 177.3 277.7 144 207.3L193.3 167c13.7-11.2 18.4-30 11.6-46.3l-40-96z"/></svg>
+                    <p class="pl-2"> {{ clickedItem.phoneNumber }}</p>
+                  </v-row>
+              </v-col>
+             </v-row>
+             <v-row>
                 <v-col>
-                  <p>Location: {{ clickedItem.location }}</p>
-                  <p>Email: {{ clickedItem.email }}</p>
-                  <p>Phone Number: {{ clickedItem.phoneNumber }}</p>
+                  <v-card   class="pb-8 pt-2 px-2">
+                    <p>Status</p>
+                    <v-chip :color="getStatusColor(clickedItem.status)" dark>{{ clickedItem.status }}</v-chip>
+                  </v-card>
+                  <v-card class="my-2 px-2 py-2">
+                    <p>CV: {{ clickedItem.cv }}</p>
+                  </v-card>
+                  <v-card class="my-2 px-2 py-2">
+                    <p>Source: {{ clickedItem.source }}</p>
+                  </v-card>
                 </v-col>
+                <v-col cols="9">
+                  <v-card >
+                    <p class="px-2 py-2">Overall Feedback: {{ clickedItem.overAllFeedBack }}</p>
+                  </v-card>
+              </v-col>
               </v-row>
             </v-col>
-            <v-card-text>
-            <p>Current Client: {{ clickedItem.currentClient }}</p>
-            <p>Foreign Name: {{ clickedItem.foreignName }}</p>
-            
-            <p>CV: {{ clickedItem.cv }}</p>
-            <p>Source: {{ clickedItem.source }}</p>
-            <p>Status: {{ clickedItem.status }}</p>
-            <p>Overall Feedback: {{ clickedItem.overAllFeedBack }}</p>
-              <!-- Display other item information here -->
-            </v-card-text>
             <v-card-actions>
               <v-spacer></v-spacer>
               <v-btn color="blue-darken-1" variant="text" @click="infoDialog = false">
@@ -309,6 +325,7 @@ export default {
           position: 'cvent developer',
           location: "Addis Ababa",
           status: "Interviewed",
+          overAllFeedBack: "over all feedback about his person spans 12 out of 12 columns on extra small to small screens, 6 out of 12 columns on small to medium screens, and 4 out of 12 columns on medium and larger screens. If you want to increase the width, you can adjust these values. For instance, if you want the textarea to take up half the screen on medium and larger screens, you"
         },
         {
           firstName: "Meron",
