8e422c299213a27d644526d5f4c5d32803119028
fevent07 <feventsige.gg@gmail.com>
2023-12-11 04:25:22 -0500

    skills job schema updates

--- backend/models/jobModel.js
+++ backend/models/jobModel.js
@@ -1,3 +1,32 @@
+const mongoose = require('mongoose');
+
+const jobSchema = mongoose.Schema({
+  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', },
+  title: { type: String, required: true },
+  company: { type: String, required: true },
+  new: { type: Boolean, required: [true, 'This filed cant be empty'] },
+  featured: { type: Boolean, required: [true, 'This filed cant be empty'] },
+  location: { type: String },
+  job_type: { type: String },
+  experience: { type: String },
+  job_summary: { type: String },
+  postedAt: { type: Date, default: Date.now },
+  job_end_date: { type: Date, default: Date.now },
+  responsibilities: { type: String },
+  // skills: { type: String, enum: ['HTML', 'CSS', 'JavaScript', 'ReactJS'] },
+  skills: [{ type: String, enum: ['HTML', 'CSS', 'JavaScript', 'ReactJS'] }],
+  is_active: { type: Boolean, default: true },
+  is_archive: { type: Boolean, default: false },
+  created_by: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  updated_by: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+}, {
+  timestamps: true,
+});
+
+module.exports = mongoose.model('Job', jobSchema);
+
+
+
 // const mongoose = require('mongoose');
 
 // const jobSchema = mongoose.Schema({
@@ -23,33 +52,3 @@
 
 // module.exports = mongoose.model('Job', jobSchema);
 
-
-const mongoose = require('mongoose');
-
-const jobSchema = mongoose.Schema({
-  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', },
-  title: { type: String, required: true },
-  company: { type: String, required: true },
-  new: { type: Boolean, required: [true, 'This filed cant be empty'] },
-  featured: { type: Boolean, required: [true, 'This filed cant be empty'] },
-  location: { type: String },
-  job_type: { type: String },
-  experience: { type: String },
-  job_summary: { type: String },
-  postedAt: { type: Date, default: Date.now },
-  job_end_date: { type: Date, default: Date.now },
-  job_end_date: { type: Date, default: Date.now },
-  responsibilities: { type: String },
-  // skills: { type: String, enum: ['HTML', 'CSS', 'JavaScript', 'ReactJS'] },
-  skills: [{ type: String, enum: ['HTML', 'CSS', 'JavaScript', 'ReactJS'] }],
-  skills: { type: String, enum: ['HTML', 'CSS', 'JavaScript', 'ReactJS'] },
-  is_active: { type: Boolean, default: true },
-  is_archive: { type: Boolean, default: false },
-  is_archive: { type: Boolean, default: false },
-  created_by: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-  updated_by: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-}, {
-  timestamps: true,
-});
-
-module.exports = mongoose.model('Job', jobSchema);
